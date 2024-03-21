d321f9e9bd98dbc822a1ceda4f2da59c1f3048d2
Rakibur Khan <rakibur@easital.com>
2024-03-21 17:57:37 +0600

    Initial Commit

---
+++ JustAnotherWeatherApp/JustAnotherWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustAnotherWeatherAppApp.swift
+//  JustAnotherWeatherApp
+//
+//  Created by Rakibur Khan on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct JustAnotherWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
